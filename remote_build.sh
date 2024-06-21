ccc4dec158e37542505a960cca8f6b5aa3a915dc
manu-180 <manumanu97@hotmail.com>
2024-06-21 16:09:48 -0300

    Adjust workflow for Windows environment

--- remote_build.sh
+++ remote_build.sh
@@ -1,3 +1,6 @@
+# Cambiar la política de ejecución para permitir scripts en PowerShell
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
 # Activar entorno virtual de Python
 echo "Activando entorno virtual"
 py -3 -m venv .venv
