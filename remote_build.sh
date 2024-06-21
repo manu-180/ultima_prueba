d74ea42c019dd628284d2c79a7c27c276e6a4e54
manu-180 <manumanu97@hotmail.com>
2024-06-21 01:16:24 -0300

    Adjust workflow for Windows environment

--- remote_build.sh
+++ remote_build.sh
@@ -1,40 +1,40 @@
-# Cambiar directorio
-echo "Changing directory to ultima_prueba"
-cd C:\Users\Manuel\Desktop\Folder\ultima_prueba
+# # Cambiar directorio
+# echo "Changing directory to ultima_prueba"
+# cd C:\Users\Manuel\Desktop\Folder\ultima_prueba
 
 echo "activando variable de entorno " 
 py -3 -m venv .venv
 .venv\Scripts\activate
 # Actualizar pip
-echo "Updating pip"
+echo "actualizando pip"
 C:\Users\Manuel\Desktop\Folder\ultima_prueba\entorno_con_3.11\Scripts\python -m pip install --upgrade pip
 
 # Instalar requerimientos
-echo "Installing requirements"
+echo "instalando requirements"
 pip install -r "requirements.txt"
 
 # Inicializar reflex
-echo "Initializing reflex"
+echo "Inicializando reflex"
 reflex init
 
 # Exportar solo el frontend de reflex
-echo "Exporting reflex frontend"
+echo "Exportando reflex frontend"
 reflex export --frontend-only
 
 # Eliminar el directorio public si existe
-echo "Removing public directory"
+echo "Removiendo public directory"
 Remove-Item -Recurse -Force public
 
 # Crear un nuevo directorio public
-echo "Creating public directory"
+echo "Creando public directory"
 mkdir public
 
 # Extraer el contenido de frontend.zip a public
-echo "Extracting frontend.zip to public"
+echo "Extrayendo frontend.zip to public"
 Expand-Archive -Path frontend.zip -DestinationPath public
 
 # Eliminar el archivo frontend.zip
-echo "Removing frontend.zip"
+echo "Removiendo frontend.zip"
 Remove-Item -Force frontend.zip
 
 # Añadir cambios a git
