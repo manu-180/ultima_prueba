bc1f0da872df35e38e7f472c4870c59558c2cc40
manu-180 <manumanu97@hotmail.com>
2024-06-21 00:45:17 -0300

    prueba2

--- remote_build.sh
+++ remote_build.sh
@@ -1,12 +1,47 @@
-cd ultima_prueba
-C:\Users\Manuel\Desktop\Folder\x\entorno_con_3.11\Scripts\python.exe -m pip install --upgrade pip 
+# Cambiar directorio
+echo "Changing directory to ultima_prueba"
+cd C:\Users\Manuel\Desktop\Folder\ultima_prueba
+
+# Actualizar pip
+echo "Updating pip"
+C:\Users\Manuel\Desktop\Folder\x\entorno_con_3.11\Scripts\python.exe -m pip install --upgrade pip
+
+# Instalar requerimientos
+echo "Installing requirements"
 pip install -r "requirements.txt"
+
+# Inicializar reflex
+echo "Initializing reflex"
 reflex init
+
+# Exportar solo el frontend de reflex
+echo "Exporting reflex frontend"
 reflex export --frontend-only
+
+# Eliminar el directorio public si existe
+echo "Removing public directory"
 Remove-Item -Recurse -Force public
+
+# Crear un nuevo directorio public
+echo "Creating public directory"
 mkdir public
+
+# Extraer el contenido de frontend.zip a public
+echo "Extracting frontend.zip to public"
 Expand-Archive -Path frontend.zip -DestinationPath public
+
+# Eliminar el archivo frontend.zip
+echo "Removing frontend.zip"
 Remove-Item -Force frontend.zip
+
+# Añadir cambios a git
+echo "Adding changes to git"
 git add .
+
+# Hacer commit de los cambios
+echo "Committing changes"
 git commit -m "Adjust workflow for Windows environment"
+
+# Hacer push de los cambios
+echo "Pushing changes"
 git push
