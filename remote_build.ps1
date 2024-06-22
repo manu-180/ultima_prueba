df74cfe5b80d0dc350b65099b49477f604d38e4d
manu-180 <manumanu97@hotmail.com>
2024-06-22 14:57:01 -0300

    Adjust workflow for Windows environment

--- remote_build.ps1
+++ remote_build.ps1
@@ -1,49 +1,53 @@
-
 # Activar entorno virtual de Python
-echo "Activando entorno virtual"
+Write-Host "Activando entorno virtual"
 py -3 -m venv .venv
 .\.venv\Scripts\Activate.ps1
 
 # Actualizar pip
-echo "Actualizando pip"
+Write-Host "Actualizando pip"
 python -m pip install --upgrade pip
 
 # Instalar requerimientos
-echo "Instalando requirements"
+Write-Host "Instalando requirements"
 pip install -r "requirements.txt"
 
 # Inicializar reflex
-echo "Inicializando reflex"
+Write-Host "Inicializando reflex"
 reflex init
 
 # Exportar solo el frontend de reflex
-echo "Exportando reflex frontend"
+Write-Host "Exportando reflex frontend"
 reflex export --frontend-only
 
-# Eliminar el directorio public si existe
-echo "Removiendo public directory"
-Remove-Item -Recurse -Force public
+# Listar archivos en el directorio actual para depuración
+Write-Host "Listando archivos en el directorio actual después de reflex export"
+Get-ChildItem
 
-# Crear un nuevo directorio public
-echo "Creando public directory"
-mkdir public
-
-# Extraer el contenido de frontend.zip a public
-echo "Extrayendo frontend.zip to public"
-Expand-Archive -Path frontend.zip -DestinationPath public
-
-# Eliminar el archivo frontend.zip
-echo "Removiendo frontend.zip"
-Remove-Item -Force frontend.zip
+# Verificar si frontend.zip existe
+if (Test-Path -Path "frontend.zip") {
+    Write-Host "frontend.zip encontrado, extrayendo archivos"
+    # Crear el directorio public si no existe
+    if (!(Test-Path -Path "public")) {
+        New-Item -ItemType Directory -Path "public"
+    }
+    # Extraer el contenido de frontend.zip a public
+    Expand-Archive -Path "frontend.zip" -DestinationPath "public"
+    # Eliminar el archivo frontend.zip
+    Write-Host "Removiendo frontend.zip"
+    Remove-Item -Path "frontend.zip" -Force
+} else {
+    Write-Host "Error: frontend.zip no encontrado, abortando"
+    exit 1
+}
 
 # Añadir cambios a git
-echo "Adding changes to git"
+Write-Host "Adding changes to git"
 git add .
 
 # Hacer commit de los cambios
-echo "Committing changes"
+Write-Host "Committing changes"
 git commit -m "Adjust workflow for Windows environment"
 
 # Hacer push de los cambios
-echo "Pushing changes"
+Write-Host "Pushing changes"
 git push
