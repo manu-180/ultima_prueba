ed171ad99b0986997948d4fcaac459bf5520ca2e
manu-180 <manumanu97@hotmail.com>
2024-06-23 19:15:14 -0300

    cambios rem

--- remote_build.ps1
+++ remote_build.ps1
@@ -1,29 +1,29 @@
-# # Activar entorno virtual de Python
-# Write-Host "Activando entorno virtual"
-# python -m venv entorno_con_3.11
-# .\entorno_con_3.11\Scripts\Activate
+# Activar entorno virtual de Python
+Write-Host "Activando entorno virtual"
+python -m venv entorno_con_3.11
+.\entorno_con_3.11\Scripts\Activate
 
-# # Actualizar pip
-# Write-Host "Actualizando pip"
-# python -m pip install --upgrade pip
+# Actualizar pip
+Write-Host "Actualizando pip"
+python -m pip install --upgrade pip
 
-# # Instalar requerimientos
-# Write-Host "Instalando requirements"
-# pip install -r requirements.txt
+# Instalar requerimientos
+Write-Host "Instalando requirements"
+pip install -r requirements.txt
 
-# # Inicializar reflex
-# Write-Host "Inicializando reflex"
-# reflex init
+# Inicializar reflex
+Write-Host "Inicializando reflex"
+reflex init
 
-# # Configurar variable de entorno API_URL
-# Write-Host "Configurando variable de entorno API_URL"
-# $env:API_URL = "https://api.baackend.com"
+# Configurar variable de entorno API_URL
+Write-Host "Configurando variable de entorno API_URL"
+$env:API_URL = "https://api.baackend.com"
 
-# # Exportar solo el frontend de reflex y capturar la salida
-# Write-Host "Exportando reflex frontend"
-# $exportOutput = & reflex export --frontend-only 2>&1
-# Write-Host "Salida de reflex export:"
-# Write-Host $exportOutput
+# Exportar solo el frontend de reflex y capturar la salida
+Write-Host "Exportando reflex frontend"
+$exportOutput = & reflex export --frontend-only 2>&1
+Write-Host "Salida de reflex export:"
+Write-Host $exportOutput
 
 # # Verificar y listar archivos en el directorio raíz después de reflex export
 # Write-Host "Listando archivos en el directorio raíz después de reflex export"
@@ -59,14 +59,14 @@
 # Write-Host "Removiendo frontend.zip"
 # Remove-Item -Force $zipFile.FullName
 
-# Añadir cambios a git
-Write-Host "Adding changes to git"
-git add .
+# # Añadir cambios a git
+# Write-Host "Adding changes to git"
+# git add .
 
-# Hacer commit de los cambios
-Write-Host "Committing changes"
-git commit -m "cambios remotos"
+# # Hacer commit de los cambios
+# Write-Host "Committing changes"
+# git commit -m "cambios remotos"
 
-# Hacer push de los cambios
-Write-Host "Pushing changes"
-git push
+# # Hacer push de los cambios
+# Write-Host "Pushing changes"
+# git push
