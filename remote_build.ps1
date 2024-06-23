2390bc3096a0a55a841e4c83f7a1b3ed063004c3
manu-180 <manumanu97@hotmail.com>
2024-06-22 22:24:36 -0300

    Update static build

--- remote_build.ps1
+++ remote_build.ps1
@@ -1,14 +1,72 @@
-# python -m venv entorno_con_3.11
-#  .\entorno_con_3.11\Scripts\Activate
+# Activar entorno virtual de Python
+Write-Host "Activando entorno virtual"
+python -m venv entorno_con_3.11
+.\entorno_con_3.11\Scripts\Activate
+
+# Actualizar pip
+Write-Host "Actualizando pip"
 python -m pip install --upgrade pip
+
+# Instalar requerimientos
+Write-Host "Instalando requirements"
 pip install -r requirements.txt
+
+# Inicializar reflex
+Write-Host "Inicializando reflex"
 reflex init
-reflex export --frontend-only
+
+# Configurar variable de entorno API_URL
+Write-Host "Configurando variable de entorno API_URL"
+Set-Item Env:API_URL "https://api.baackend.com"
+
+# Exportar solo el frontend de reflex y capturar la salida
+Write-Host "Exportando reflex frontend"
+$exportOutput = & reflex export --frontend-only 2>&1
+Write-Host "Salida de reflex export:"
+Write-Host $exportOutput
+
+# Desactivar entorno virtual
+Write-Host "Desactivando entorno virtual"
+deactivate
+
+# Listar archivos en el directorio actual después de exportar
+Write-Host "Listando archivos en el directorio actual después de reflex export"
+Get-ChildItem
+
+# Verificar si frontend.zip existe en cualquier parte del proyecto
+Write-Host "Verificando la existencia de frontend.zip en cualquier parte del proyecto"
+$zipFile = Get-ChildItem -Recurse -Filter "frontend.zip" | Select-Object -First 1
+if ($zipFile) {
+    Write-Host "frontend.zip encontrado en: $($zipFile.FullName)"
+} else {
+    Write-Host "frontend.zip no encontrado, abortando"
+    exit 1
+}
+
+# Eliminar el directorio public si existe
+Write-Host "Removiendo public directory"
 Remove-Item -Recurse -Force public
-mkdir public
-Expand-Archive -Path frontend.zip -DestinationPath public
-Remove-Item -Force frontend.zip
-# deactivate
+
+# Crear un nuevo directorio public
+Write-Host "Creando public directory"
+New-Item -ItemType Directory -Path public
+
+# Extraer el contenido de frontend.zip a public
+Write-Host "Extrayendo archivos de frontend.zip a public"
+Expand-Archive -Path $zipFile.FullName -DestinationPath public
+
+# Eliminar el archivo frontend.zip
+Write-Host "Removiendo frontend.zip"
+Remove-Item -Force $zipFile.FullName
+
+# Añadir cambios a git
+Write-Host "Adding changes to git"
 git add .
-git commit -m "cambios"
-git push 
+
+# Hacer commit de los cambios
+Write-Host "Committing changes"
+git commit -m "Update static build"
+
+# Hacer push de los cambios
+Write-Host "Pushing changes"
+git push
