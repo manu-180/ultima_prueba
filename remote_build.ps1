4aeb31e18b8b4eeb68c8d5ebea4b4abdf64bab5d
manu-180 <manumanu97@hotmail.com>
2024-06-22 21:55:10 -0300

    cambios

--- remote_build.ps1
+++ remote_build.ps1
@@ -4,10 +4,10 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
-Remove-Item -Recurse -Force public
-mkdir public
-Expand-Archive -Path frontend.zip -DestinationPath public
-Remove-Item -Force frontend.zip
+# Remove-Item -Recurse -Force public
+# mkdir public
+# Expand-Archive -Path frontend.zip -DestinationPath public
+# Remove-Item -Force frontend.zip
 deactivate
 git add .
 git commit -m "cambios"
