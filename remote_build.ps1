bf86e8cfd35e0b42b09d03dec993b2bf08ce1dd1
manu-180 <manumanu97@hotmail.com>
2024-06-22 22:10:40 -0300

    cambios

--- remote_build.ps1
+++ remote_build.ps1
@@ -3,8 +3,7 @@ python -m venv entorno_con_3.11
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
-reflex export --frontend-only
-# Remove-Item -Recurse -Force public
+API_URL=https://api.baackend.com reflex export --frontend-only# Remove-Item -Recurse -Force public
 # mkdir public
 # Expand-Archive -Path frontend.zip -DestinationPath public
 # Remove-Item -Force frontend.zip
