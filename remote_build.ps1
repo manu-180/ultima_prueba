7c803328a60461377c1d1c3fef3e85f4893d7ad8
manu-180 <manumanu97@hotmail.com>
2024-06-22 22:13:53 -0300

    cambios

--- remote_build.ps1
+++ remote_build.ps1
@@ -1,14 +1,14 @@
-python -m venv entorno_con_3.11
- .\entorno_con_3.11\Scripts\Activate
+# python -m venv entorno_con_3.11
+#  .\entorno_con_3.11\Scripts\Activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
-# Remove-Item -Recurse -Force public
-# mkdir public
-# Expand-Archive -Path frontend.zip -DestinationPath public
-# Remove-Item -Force frontend.zip
-deactivate
+Remove-Item -Recurse -Force public
+mkdir public
+Expand-Archive -Path frontend.zip -DestinationPath public
+Remove-Item -Force frontend.zip
+# deactivate
 git add .
 git commit -m "cambios"
 git push 
